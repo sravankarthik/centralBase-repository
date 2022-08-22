3c347329508f51d005162749658f1d669833b702
gptutk <111006161+gptutk@users.noreply.github.com>
2022-08-22 16:23:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = require("path");
 const crypto = require("crypto");
 const mongoose = require("mongoose");
 const multer = require("multer");
-const GridFsStrorage = require("multer-gridfs-storage");
+const {GridFsStorage} = require("multer-gridfs-storage");
 const Grid = require("gridfs-stream");
 const methodOverride = require("method-override");
 
@@ -22,6 +22,44 @@ const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const jobRoutes = require("./routes/job");
 const assignmentRoutes = require("./routes/assignment");
+
+//file uploads.
+const mongoULI = 'mongodb://localhost:27017/test1';
+const conn = mongoose.createConnection(mongoURI);
+let gfs;
+conn.once("open", () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+  //collection name
+  gfs.collection("uploads");
+});
+
+
+const storage = new GridFsStrorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+            //collection name to be specified in bucketName
+          bucketName: "uploads",
+        };
+        resolve(fileInfo);
+      });
+    });
+  },
+});
+const upload = multer({ storage });
+
+app.post("/upload", upload.single("file"), (req, res) => {
+    //to me modified later. This is for single file.
+  res.json({ file: req.file });
+});
+
 
 //Routes
 app.use("/api", authRoutes);
